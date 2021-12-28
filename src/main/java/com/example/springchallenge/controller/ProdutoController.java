943acb5f1364e4d9c8e60d204965f63391034d30
Lucca Gabriel Dias Barbosa <lucca.dias@mercadolivre.com>
2021-12-28 16:29:13 -0300

    ProdutoController - Removendo rota do RequestMapping

--- src/main/java/com/example/springchallenge/controller/ProdutoController.java
+++ src/main/java/com/example/springchallenge/controller/ProdutoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/insert-articles-request")
+@RequestMapping()
 public class ProdutoController {
 
     @GetMapping("/ping")
