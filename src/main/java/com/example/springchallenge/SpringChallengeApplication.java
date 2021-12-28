1e17af5c5b7454af1f048e42646ce3a2344b4b89
Lucca Gabriel Dias Barbosa <lucca.dias@mercadolivre.com>
2021-12-28 15:40:36 -0300

    Primeiro Commit

---
+++ src/main/java/com/example/springchallenge/SpringChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springchallenge/SpringChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
