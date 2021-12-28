c4f198040a4c0f523c7af42e8c34ed87f1eacaa1
Lucca Gabriel Dias Barbosa <lucca.dias@mercadolivre.com>
2021-12-28 19:02:06 -0300

    Carregar e inserir produtos em arquivo

--- src/main/java/com/example/springchallenge/repository/ProdutoRepository.java
+++ src/main/java/com/example/springchallenge/repository/ProdutoRepository.java
@@ -1,4 +1,68 @@
 package com.example.springchallenge.repository;
 
-public class ProdutoRepository {
+import com.example.springchallenge.entity.Produto;
+import com.example.springchallenge.repository.interfaces.BaseRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.stereotype.Repository;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Repository
+public class ProdutoRepository implements BaseRepository<Produto, Long> {
+    private ObjectMapper objectMapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+    private final String PATH = "usuarios.json";
+    private List<Produto> produtos = new ArrayList<>(loadAllFromFile());
+
+    @Override
+    public void save(Produto produto) {
+
+    }
+
+    @Override
+    public List<Produto> getAll() {
+        return new ArrayList<>(produtos);
+    }
+
+    @Override
+    public Produto getById(Long id) {
+        return null;
+    }
+
+    public List<Produto> insertAllArticles(List<Produto> produtos) {
+        for (Produto produto : produtos) {
+            produto.setProductId((long) this.produtos.size() + 1);
+            this.produtos.add(produto);
+        }
+
+        updateFile();
+        return produtos;
+    }
+
+    private void updateFile() {
+        try {
+            objectMapper.writeValue(new File(PATH), this.produtos);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private List<Produto> loadAllFromFile() {
+        List<Produto> produtos = new ArrayList<>();
+
+        try {
+            File file = new File(PATH);
+            FileInputStream fileInputStream = new FileInputStream(file);
+            produtos = Arrays.asList(objectMapper.readValue(fileInputStream, Produto[].class));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return produtos;
+    }
 }
