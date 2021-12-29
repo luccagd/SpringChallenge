e79b65705712db62428568c09e2ef44ac6dbecb9
avelinovinicius <vinicius.avelino@mercadolivre.com>
2021-12-29 11:22:44 -0300

    List by Higher and lower price

--- src/main/java/com/example/springchallenge/repository/ProdutoRepository.java
+++ src/main/java/com/example/springchallenge/repository/ProdutoRepository.java
@@ -1,4 +1,35 @@
 package com.example.springchallenge.repository;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.example.springchallenge.entity.Produto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+@Component
 public class ProdutoRepository {
+	private final String PATH = "database.json";
+	private ObjectMapper objectMapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+	private List<Produto> produtos = new ArrayList<>();
+
+	public List<Produto> retornaProdutos() throws IOException {
+		File file = new File(PATH);
+		FileInputStream is = new FileInputStream(file);
+		produtos = Arrays.asList(objectMapper.readValue(is, Produto[].class));
+		return produtos;
+
+	}
+
+	public List<Produto> getProductsByCategoryAndFreeShippingAndOrder(String categoria, Boolean freeShipping, int order) throws IOException {
+		return retornaProdutos();
+
+	}
+
 }
